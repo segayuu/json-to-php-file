6ac97064b15d22958059b49604a6923bedffb72e
segayuu <segayuu@gmail.com>
2024-01-28 22:20:49 +0900

    Refacror eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,33 +1,19 @@
+// @ts-check
 /** @typedef {import("eslint").Linter.FlatConfig} FlatConfig */
 import js from "@eslint/js";
-import typescriptParser from "@typescript-eslint/parser";
-import typescriptPlugin from "@typescript-eslint/eslint-plugin";
 import { FlatCompat } from "@eslint/eslintrc";
-const compat = new FlatCompat({
-  resolvePluginsRelativeTo: import.meta.dirname,
-});
+const compat = new FlatCompat();
 
 /** @type {FlatConfig[]} */
 export default [
+  js.configs.recommended,
+  ...compat.extends("plugin:@typescript-eslint/recommended-type-checked"),
   {
-    files: ["./**/*.mjs", "./src/index.mts"],
     languageOptions: {
       ecmaVersion: 2023,
-      sourceType: "module",
-    },
-    rules: js.configs.recommended.rules,
-  },
-  ...compat.extends("plugin:@typescript-eslint/recommended-type-checked"),
-  // Enable TypeScript-specific rules
-  {
-    files: ["./src/index.mts"],
-    plugins: {
-      "@typescript-eslint": typescriptPlugin,
-    },
-    languageOptions: {
-      parser: typescriptParser,
       parserOptions: {
-        project: "./tsconfig.json",
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
       },
     },
   },
